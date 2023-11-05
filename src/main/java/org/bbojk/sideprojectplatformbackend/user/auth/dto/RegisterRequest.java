ddfde70d0717b71d8f526277af57ee68bca9160d
ysndy <ysndy@naver.com>
2023-11-05 14:10:12 +0900

    feat:#1 회원가입 & 로그인 에 대한 컨트롤러, 서비스, DTO 구현

---
+++ src/main/java/org/bbojk/sideprojectplatformbackend/user/auth/dto/RegisterRequest.java
@@ -0,0 +1,17 @@
+package org.bbojk.sideprojectplatformbackend.user.auth.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class RegisterRequest {
+
+    private String email;
+    private String password;
+
+}
