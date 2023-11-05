8ffee49b2fb9c768653c23e8587853fac8374b0d
ysndy <ysndy@naver.com>
2023-11-05 14:09:01 +0900

    feat:#1 configuration 구현

---
+++ src/main/java/org/bbojk/sideprojectplatformbackend/user/config/AuthenticationConfig.java
@@ -0,0 +1,52 @@
+package org.bbojk.sideprojectplatformbackend.user.config;
+
+import lombok.RequiredArgsConstructor;
+import org.bbojk.sideprojectplatformbackend.security.AppUserDetailsService;
+import org.bbojk.sideprojectplatformbackend.user.domain.AppUserRepository;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@RequiredArgsConstructor
+public class AuthenticationConfig {
+
+    private final AppUserDetailsService appUserDetailsService;
+
+    @Bean
+    public UserDetailsService userDetailsService(){
+        return appUserDetailsService;
+    }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider(){
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+        authProvider.setUserDetailsService(userDetailsService());
+        authProvider.setPasswordEncoder(passwordEncoder());
+        return authProvider;
+    }
+
+    /**
+     *
+     * @param config //스프링부트의 기본 제공 빈
+     * @return
+     * @throws Exception
+     */
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+        return config.getAuthenticationManager();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+}
